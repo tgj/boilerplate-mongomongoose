2433d8aef1b2d04ecb23074f3766bee4aef13ff5
Tyler Johnston <tylergarrettjohnston@gmail.com>
2024-06-01 07:27:20 -0400

    Clean up code

--- myApp.js
+++ myApp.js
@@ -6,16 +6,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 });
 
-let personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  age: Number,
-  favoriteFoods: [String],
-});
-
-let Person = mongoose.model("Person", personSchema);
+let Person = require("./models/person");
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
